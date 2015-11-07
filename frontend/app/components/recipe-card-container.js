28c30d6ffb2dff1e540c7e01daf270fe019cffdb
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-11-07 10:30:56 -0500

    Set isActive to only one recipe

--- frontend/app/components/recipe-card-container.js
+++ frontend/app/components/recipe-card-container.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+
+	/**
+	* Properties
+	*/
+	recipes: null,
+
+	currentRecipe: function () {
+
+		return this.get("recipes").get("firstObject");
+	}.property("recipes.[]")
+
+
+	/**
+	* Methods
+	*/
+
+
+
 });
--- frontend/app/components/recipe-card.js
+++ frontend/app/components/recipe-card.js
@@ -2,10 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-	classNames: ["recipe-card"]
+	classNames: ["recipe-card"],
+	classNameBindings: ["isActive:m-active"],
+
 	/**
 	* Properties
 	*/
+	recipe: null,
+	currentRecipe: null,
+
+	isActive: function () {
+
+		let recipeId = this.get("recipe.id"),
+				currentRecipeId = this.get("currentRecipe.id");
+
+		if (recipeId === currentRecipeId) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}.property("recipe", "currentRecipe")
 	
 
 });
