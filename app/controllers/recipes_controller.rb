32d2a6f81bc21b97398fe3a2bfd9d1f40966f4b2
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-11-08 19:43:49 -0500

    minor changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,7 @@ class RecipesController < ApplicationController
   end
 
   def recipes_query
-    @q = Recipe.ransack(params[:query])
+    @q = Recipe.ransack(ingredient_lines_cont: params[:query])
     @recipes = @q.result(distinct: true)
 
     render json: @recipes
