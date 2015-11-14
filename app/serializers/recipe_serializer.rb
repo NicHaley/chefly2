401b91d97ff02f572a508a00f9a3b36559fc2ed5
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-11-14 11:21:59 -0500

    Send associated ingredients to ember in json

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,10 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :cook_time, :yield, :servings
+  attributes :id, :name, :cook_time, :yield, :servings, :ingredients
+
+  def ingredients
+    object.ingredients.map do |ingredient|
+      IngredientSerializer.new(ingredient, scope: scope, root: false, event: object)
+    end
+  end
+
 end
