26b56eb7117c38174d0963618a9b23e05e078dac
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-12-06 10:22:15 -0500

    image button swiping works

--- frontend/app/components/swipe-container.js
+++ frontend/app/components/swipe-container.js
@@ -2,8 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+	/**
+	* Properties
+	*/
 	classNames: ["b-swipe-container"],
 	b: "b-swipe-container",
+
+	activeRecipe: null,
+	imageIndex: null,
+
+	lastImage: function () {
+
+		return this.get("activeRecipe.image_urls.length") - 1;
+
+	}.property("activeRecipe.image_urls"),
+
 
 	/**
 	* Actions
@@ -22,6 +35,21 @@ export default Ember.Component.extend({
      */
     switchRecipe (direction) {
     	this.sendAction("switchRecipe", direction);
+    },
+
+
+    /**
+     *
+     */
+    cycleImage (direction) {
+
+    	let imageIndex = this.get("imageIndex");
+
+    	if (direction === "up") {
+    		this.set("imageIndex", imageIndex - 1)
+    	} else if (direction === "down") {
+				this.set("imageIndex", imageIndex + 1)
+    	}
     }
 	}
 
